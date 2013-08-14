aec94ea8f502dfba46f4aa79c649427e91177b95
Keisuke KITA <kei.kita2501@gmail.com>
2013-08-14 11:09:01 +0900

    コメントを追記

--- lib/about_p/command.rb
+++ lib/about_p/command.rb
@@ -9,6 +9,9 @@ module AboutP
     end
 
     class << self
+      # default_command で指定しても「aboutp クエリ」とかで呼び出すと
+      # クエリがコマンドだと認識されるので、
+      # handle_no_command_errorをハンドリングする
       def handle_no_command_error(command, has_namespace = $thor_runner)
         Command.new.search(command)
       end
